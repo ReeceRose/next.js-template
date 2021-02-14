815d554d3f43cf73261855304454fdc69df30f62
Reece Rose <reecerose18@gmail.com>
2021-02-13 21:10:45 -0500

    Created basic project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Next.js Template
+
+More information coming soon
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Template</title>
+      </Head>
+
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
