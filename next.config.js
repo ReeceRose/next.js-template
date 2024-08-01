18a30806267655fba5d406a3525899ccaec0ff6c
Reece Rose <reecerose18@gmail.com>
2024-07-31 21:45:04 -0400

    Package upgrades

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  experimental: {
-    // Required:
-    appDir: true,
-  },
+  experimental: {},
 };
 
 module.exports = nextConfig;
